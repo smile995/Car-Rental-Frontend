95b0433c1869814318021ae4190300d0626f29fb
smile995 <ah.smile995@gmail.com>
2025-08-14 14:29:50 +0600

    Add why choose us section in homepage

--- src/app/(commonLayout)/components/page/home/WhyChooseUs.tsx
+++ src/app/(commonLayout)/components/page/home/WhyChooseUs.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Title from '../shared/Title'
+import React from "react";
+import Title from "../shared/Title";
+import { whyChooseUs } from "../../../../../../public/DummyData/WhyChooseUs";
 
 const WhyChooseUs = () => {
   return (
     <div>
-     <Title heading='Why Choose Us' subheading='Discover what makes us the trusted choice for car buyers around the world.'/>
+      <Title
+        heading="Why Choose Us"
+        subheading="Discover what makes us the trusted choice for car buyers around the world."
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 items-center">
+        {whyChooseUs?.map((item) => (
+          <div
+            className="bg-slate-100 hover:translate-x-2 hover:border-t-5 hover:border-blue-500 hover:translate-y-2 shadow-md rounded-lg p-5 hover:shadow-red-500 flex items-start text-center h-52"
+            key={item.id}
+          >
+            <div className="space-y-3">
+              <h1 className="text-3xl">{item.icon}</h1>
+              <p className="font-semibold text-xl">{item.title}</p>
+              <p className="text-slate-700">{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhyChooseUs
+export default WhyChooseUs;
